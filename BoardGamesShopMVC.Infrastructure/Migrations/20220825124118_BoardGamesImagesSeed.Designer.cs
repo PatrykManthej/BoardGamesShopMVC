650b4cf484b79cb1232ab24f401b180d0c1aee84
manpat <manpat1997@gmail.com>
2022-08-25 14:53:18 +0200

    board games images seed

--- BoardGamesShopMVC.Infrastructure/Migrations/20220825124118_BoardGamesImagesSeed.Designer.cs
+++ BoardGamesShopMVC.Infrastructure/Migrations/20220825124118_BoardGamesImagesSeed.Designer.cs
@@ -4,6 +4,7 @@ using BoardGamesShopMVC.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace BoardGamesShopMVC.Infrastructure.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20220825124118_BoardGamesImagesSeed")]
+    partial class BoardGamesImagesSeed
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ BoardGamesShopMVC.Infrastructure/Migrations/20220825124118_BoardGamesImagesSeed.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BoardGamesShopMVC.Infrastructure.Migrations
+{
+    public partial class BoardGamesImagesSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "BoardGames",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "\\images\\boardgames\\24cf50d0-b7ed-44f5-8269-20889c9ca1ba_Carcassonne.png");
+
+            migrationBuilder.UpdateData(
+                table: "BoardGames",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "\\images\\boardgames\\73c2c91e-e0ba-4405-98f2-4dc99f90229b_Splendor.jpg");
+
+            migrationBuilder.UpdateData(
+                table: "BoardGames",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "\\images\\boardgames\\06718f78-7290-48fe-b889-34f48cd3cdfc_Nemesis.png");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "BoardGames",
+                keyColumn: "Id",
+                keyValue: 1,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BoardGames",
+                keyColumn: "Id",
+                keyValue: 2,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.UpdateData(
+                table: "BoardGames",
+                keyColumn: "Id",
+                keyValue: 3,
+                column: "ImageUrl",
+                value: "");
+        }
+    }
+}
