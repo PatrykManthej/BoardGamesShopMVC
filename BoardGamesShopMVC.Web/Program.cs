0acac721b34eed443a111ebfce2b61ef132ce301
manpat <manpat1997@gmail.com>
2022-09-13 15:10:35 +0200

    log database commands added

--- BoardGamesShopMVC.Web/Program.cs
+++ BoardGamesShopMVC.Web/Program.cs
@@ -11,7 +11,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<Context>(options =>
-    options.UseSqlServer(connectionString));
+{
+    options.UseSqlServer(connectionString);
+    options.LogTo(Console.WriteLine, new[] { DbLoggerCategory.Database.Command.Name }, LogLevel.Information).EnableSensitiveDataLogging();
+}
+    );
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
