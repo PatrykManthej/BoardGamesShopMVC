e0e23963a35994a042a53224c96c25746254e0e1
Patryk Manthej <patrykmanthej@gmail.com>
2022-12-15 15:09:05 +0100

    added email property to OrderRecipient

--- BoardGamesShopMVC.Domain/Model/Order/OrderRecipient.cs
+++ BoardGamesShopMVC.Domain/Model/Order/OrderRecipient.cs
@@ -3,6 +3,7 @@
     public class OrderRecipient
     {
         public int Id { get; set; }
+        public string Email { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public string PhoneNumber { get; set; }
